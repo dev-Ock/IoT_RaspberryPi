49f632959e284b3e4369cb06d44426b2fc57201c
_devOck <respecttowardyou@gmail.com>
2022-11-16 10:07:01 +0900

    main페이지 작성

--- main.py
+++ main.py
@@ -2,34 +2,22 @@ import requests
 import json
 from requests_toolbelt import MultipartEncoder
 
-url = "http://192.168.0.16:3003/camera/image"
 
-headers = {
-    # "Content-Type": "application/json"
-    "Content-Type": "multipart/form-data"
-}
+def photo_image(key):
+    
+    url = "http://192.168.0.16:3003/camera/image"
 
-# data = {
-#   "name" : "kk"
-# }
+    payload = MultipartEncoder(
+        fields={'key': key,
+                'img' : ('image.jpg', open('image/image.jpg', 'rb'), 'text/plain'),
+                }
+    )
 
-# data = json.dumps(data)
-
-files = open('/home/jaehyeong/project/image/image.jpg', 'rb')
-m = MultipartEncoder(fields = {'field0' : files})
-upload = {'file3':m} 
-
-print("이미지 : ",m)
-
-res = requests.post(url,  headers={'Content-Type': m.content_type}, files = upload)
-if res.status_code == 200:
-     print("Success!!")
-     print(type(files))
-      
-      
-      
-# def post(url, field_data) :
-#     m = MultipartEncoder(fields=field_data)
-#     headers = {'Content-Type' : m.content_type}
-#     res = requests.post(url, headers=headers, data=m)
-#     return res.status_code, res.json()
+    # print(payload)
+    
+    requests.post(url,  headers = {'Content-Type': payload.content_type}, data = payload)
+    if res.status_code == 200:
+             print("Success!!")
+    
+    
+photo_image('1234')
