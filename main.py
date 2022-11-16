b3a4042aadf47331eed3ec4ed3e5faf2ad7e9c17
_devOck <respecttowardyou@gmail.com>
2022-11-16 15:48:53 +0900

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+# import time
+# import picamera
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+from time import sleep
+
 import requests
 import json
 from requests_toolbelt import MultipartEncoder
 
+def main_page():
+    url = "http://192.168.0.16:3003"
+    res = requests.get(url)
+    if res.status_code == 200:
+          print("main_page")
 
+# def main_page2():
+#     url = "http://192.168.0.16:3003/index"
+#     res = requests.get(url)
+#     if res.status_code == 200:
+#           print("main_page2")
+          
 def photo_image(key):
     url = "http://192.168.0.16:3003/camera/image"
 
@@ -17,6 +34,47 @@ def photo_image(key):
     res = requests.post(url, headers = {'Content-Type': payload.content_type}, data = payload)
     if res.status_code == 200:
              print("Success!!")
+             
+button_pin = 27
+camera = PiCamera()
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(button_pin, GPIO.IN)
+# GPIO.setup(camera, GPIO.OUT)
+
+buttonInputPrev = False
+# cameraOn = False
+
+try:
+  while True:
+    buttonInput = GPIO.input(button_pin)
     
+    if buttonInput and not buttonInputPrev:
+        print("rising edge")
+        # cameraOn = Ture if not cameraOn else False
+        # GPIO.output(camera, cameraOn)
+        
+        camera.start_preview()
+        # main_page2()
+        sleep(5)
+        camera.capture('/home/jaehyeong/project/image/image.jpg')
+        camera.stop_preview()
+        photo_image('1234')
+        
+    elif not buttonInput and buttonInputPrev:
+        main_page()      
+        print("falling edge")
+    else: pass
     
-photo_image('1234')
+    buttonInputPrev = buttonInput
+
+except KeyboardInterrupt:
+    pass
+  
+GPIO.cleanup()
+# # with picamera.PiCamera() as camera:
+# camera.start_preview()
+# GPIO.wait_for_edge(27, GPIO.FALLING)
+# camera.capture('/home/jaehyeong/project/image/image.jpg')
+# camera.stop_preview()
