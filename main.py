a1eb02676ed24d78cfce7389de2634e70a0d6d7d
_devOck <respecttowardyou@gmail.com>
2022-11-17 11:56:54 +0900

    기본 완료

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# import time
-# import picamera
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
@@ -8,28 +6,22 @@ import requests
 import json
 from requests_toolbelt import MultipartEncoder
 
-def main_page():
-    url = "http://192.168.0.16:3003"
-    res = requests.get(url)
-    if res.status_code == 200:
-          print("main_page")
-
-# def main_page2():
-#     url = "http://192.168.0.16:3003/index"
+# def main_page():
+#     url = "http://192.168.0.16:3003"
 #     res = requests.get(url)
 #     if res.status_code == 200:
-#           print("main_page2")
-          
-def photo_image(key):
-    url = "http://192.168.0.16:3003/camera/image"
+#           print("main_page")
 
+def photo_image(key):
+    print("들어옴")
+    url = "http://192.168.0.16:3003/camera/image"
+           
     payload = MultipartEncoder(
         fields={'key': key,
                 'img' : ('image.jpg', open('image/image.jpg', 'rb'), 'text/plain'),
                 }
     )
-
-    # print(payload)
+    print("들어옴2")
     
     res = requests.post(url, headers = {'Content-Type': payload.content_type}, data = payload)
     if res.status_code == 200:
@@ -37,14 +29,13 @@ def photo_image(key):
              
 button_pin = 27
 camera = PiCamera()
+# camera.rotation = 180
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(button_pin, GPIO.IN)
-# GPIO.setup(camera, GPIO.OUT)
 
 buttonInputPrev = False
-# cameraOn = False
 
 try:
   while True:
@@ -52,18 +43,14 @@ try:
     
     if buttonInput and not buttonInputPrev:
         print("rising edge")
-        # cameraOn = Ture if not cameraOn else False
-        # GPIO.output(camera, cameraOn)
-        
         camera.start_preview()
-        # main_page2()
-        sleep(5)
+        sleep(2)
         camera.capture('/home/jaehyeong/project/image/image.jpg')
         camera.stop_preview()
         photo_image('1234')
         
     elif not buttonInput and buttonInputPrev:
-        main_page()      
+        # main_page()      
         print("falling edge")
     else: pass
     
@@ -73,8 +60,3 @@ except KeyboardInterrupt:
     pass
   
 GPIO.cleanup()
-# # with picamera.PiCamera() as camera:
-# camera.start_preview()
-# GPIO.wait_for_edge(27, GPIO.FALLING)
-# camera.capture('/home/jaehyeong/project/image/image.jpg')
-# camera.stop_preview()
